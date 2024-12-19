98335886fc77e589b64be9c7a42dc97dadd35fe5
Jash Shah <jash.shah@nissan-usa.com>
2024-12-18 17:00:22 -0800

    new wihtout error

--- background.js
+++
@@ -1,9 +0,0 @@
-// Basic background script for extension functionality
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('Gmail Quote Generator installed');
-});
-
-// Keep service worker active
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  return true;
-}); 
